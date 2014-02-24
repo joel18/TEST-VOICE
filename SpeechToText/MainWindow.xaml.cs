aa2ae26bdc645ead6af6758b9053cbe22d51f326
JOEL <joel.xd18@gmail.com>
2014-02-24 17:53:32 -0500

    First version

---
+++ SpeechToText/MainWindow.xaml.cs
@@ -0,0 +1,166 @@
+﻿#region using directives
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Speech.Recognition;
+using System.Windows;
+using System.Diagnostics;
+using System.Globalization;
+
+#endregion
+
+namespace SpeechToText
+{
+    /// <summary>
+    /// MainWindow class
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region locals
+
+        /// <summary>
+        /// the engine
+        /// </summary>
+        SpeechRecognitionEngine speechRecognitionEngine = null;
+
+        /// <summary>
+        /// list of predefined commands
+        /// </summary>
+        List<Word> words = new List<Word>();
+
+        #endregion
+
+        #region ctor
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainWindow"/> class.
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            try
+            {
+                // create the engine
+                //speechRecognitionEngine = createSpeechEngine("de-DE");
+                //speechRecognitionEngine = createSpeechEngine(CultureInfo.CurrentCulture.Name); 
+                speechRecognitionEngine = createSpeechEngine("es-ES"); 
+
+                // hook to events
+                speechRecognitionEngine.AudioLevelUpdated += new EventHandler<AudioLevelUpdatedEventArgs>(engine_AudioLevelUpdated);
+
+                // Create and load a dictation grammar.
+                speechRecognitionEngine.LoadGrammar(new DictationGrammar());
+
+                speechRecognitionEngine.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(engine_SpeechRecognized);
+
+                // use the system's default microphone
+                speechRecognitionEngine.SetInputToDefaultAudioDevice();
+
+                // start listening
+                speechRecognitionEngine.RecognizeAsync(RecognizeMode.Multiple);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Voice recognition failed");
+            }
+        }
+
+        #endregion
+
+        #region internal functions and methods
+
+        /// <summary>
+        /// Creates the speech engine.
+        /// </summary>
+        /// <param name="preferredCulture">The preferred culture.</param>
+        /// <returns></returns>
+        private SpeechRecognitionEngine createSpeechEngine(string preferredCulture)
+        {
+            foreach (RecognizerInfo config in SpeechRecognitionEngine.InstalledRecognizers())
+            {
+                if (config.Culture.ToString() == preferredCulture)
+                {
+                    speechRecognitionEngine = new SpeechRecognitionEngine(config);
+                    break;
+                }
+            }
+
+            // if the desired culture is not found, then load default
+            if (speechRecognitionEngine == null)
+            {
+                MessageBox.Show("The desired culture is not installed on this machine, the speech-engine will continue using "
+                    + SpeechRecognitionEngine.InstalledRecognizers()[0].Culture.ToString() + " as the default culture.",
+                    "Culture " + preferredCulture + " not found!");
+                speechRecognitionEngine = new SpeechRecognitionEngine(SpeechRecognitionEngine.InstalledRecognizers()[0]);
+            }
+
+            return speechRecognitionEngine;
+        }
+
+        /// <summary>
+        /// Loads the grammar and commands.
+        /// </summary>
+
+        /// <summary>
+        /// Gets the known command.
+        /// </summary>
+        /// <param name="command">The order.</param>
+        /// <returns></returns>
+        
+        #endregion
+
+        #region speechEngine events
+
+        /// <summary>
+        /// Handles the SpeechRecognized event of the engine control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="System.Speech.Recognition.SpeechRecognizedEventArgs"/> instance containing the event data.</param>
+        void engine_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            txtSpoken.Text += "\r" + e.Result.Text;
+            scvText.ScrollToEnd();
+        }
+
+        /// <summary>
+        /// Handles the AudioLevelUpdated event of the engine control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="System.Speech.Recognition.AudioLevelUpdatedEventArgs"/> instance containing the event data.</param>
+        void engine_AudioLevelUpdated(object sender, AudioLevelUpdatedEventArgs e)
+        {
+            prgLevel.Value = e.AudioLevel;
+        }
+
+        #endregion
+
+        #region window closing
+
+        /// <summary>
+        /// Handles the Closing event of the Window control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="System.ComponentModel.CancelEventArgs"/> instance containing the event data.</param>
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            // unhook events
+            speechRecognitionEngine.RecognizeAsyncStop();
+            // clean references
+            speechRecognitionEngine.Dispose();
+        }
+
+        #endregion
+
+        #region GUI events
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        #endregion
+    }
+}
